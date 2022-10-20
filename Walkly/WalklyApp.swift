a4bbb94b8a7fe89a22ef9290d575e63e2715674f
yuppejp <yuppejp@gmail.com>
2022-10-20 19:40:28 +0900

    Initial Commit

---
+++ Walkly/WalklyApp.swift
@@ -0,0 +1,18 @@
+//
+//  WalklyApp.swift
+//  Walkly
+//
+//  Created on 2022/10/20.
+//  
+//
+
+import SwiftUI
+
+@main
+struct WalklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
